0a662fc35ae3a0608cf6e8e7c7418ad15f4dbf63
Mokoena-ER <221451390@edu.vut.ac.za>
2025-06-23 15:39:25 +0200

    Create a basic create account method

--- mybank/src/main/java/com/bank/mybank/controller/AccountController.java
+++ mybank/src/main/java/com/bank/mybank/controller/AccountController.java
@@ -1,4 +1,28 @@
 package com.bank.mybank.controller;
 
+import com.bank.mybank.dto.AccountReqDto;
+import com.bank.mybank.dto.AccountResDto;
+import com.bank.mybank.model.Account;
+import com.bank.mybank.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/account")
+@CrossOrigin("*")
 public class AccountController {
+
+    @Autowired
+    private final AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @PostMapping("/create")
+    public AccountResDto createAccount(@RequestBody AccountReqDto reqDto) {
+        return accountService.createAccount(reqDto);
+    }
+
+
 }
--- mybank/src/main/java/com/bank/mybank/service/AccountService.java
+++ mybank/src/main/java/com/bank/mybank/service/AccountService.java
@@ -1,4 +1,32 @@
 package com.bank.mybank.service;
 
+import com.bank.mybank.dto.AccountReqDto;
+import com.bank.mybank.dto.AccountResDto;
+import com.bank.mybank.mapper.AccountMapper;
+import com.bank.mybank.model.Account;
+import com.bank.mybank.repository.AccountRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AccountService {
+
+    @Autowired
+    private final AccountRepo accountRepo;
+
+    @Autowired
+    private AccountMapper mapper;
+
+    public AccountService(AccountRepo accountRepo, AccountMapper mapper) {
+        this.accountRepo = accountRepo;
+        this.mapper = mapper;
+    }
+
+
+    public AccountResDto createAccount(AccountReqDto reqDto) {
+        Account entity = mapper.toEntity(reqDto);
+        Account saved = accountRepo.save(entity);
+        return mapper.toResDto(saved);
+    }
+
 }
