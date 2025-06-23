2bf5132e3be0c49aaa1e56aa35508c1b0d9c1e90
Mokoena-ER <221451390@edu.vut.ac.za>
2025-06-23 15:21:42 +0200

    Update AccountRepo

--- mybank/src/main/java/com/bank/mybank/repository/AccountRepo.java
+++ mybank/src/main/java/com/bank/mybank/repository/AccountRepo.java
@@ -1,4 +1,10 @@
 package com.bank.mybank.repository;
 
-public class AccountRepo {
+import com.bank.mybank.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepo extends JpaRepository<Account, Long> {
+    Account findByAccountNumber(String accountNumber);
 }
